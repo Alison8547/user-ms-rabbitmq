3f5eb81517c0b9c9fb8dc6c168ff71934ac69b19
Alison8547 <alisonailson100@hotmail.com>
2023-10-31 22:06:55 -0300

    feat: dtos User

---
+++ src/main/java/com/ms/user/dto/response/UserResponse.java
@@ -0,0 +1,19 @@
+package com.ms.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private UUID idUser;
+    private String name;
+    private String email;
+}
